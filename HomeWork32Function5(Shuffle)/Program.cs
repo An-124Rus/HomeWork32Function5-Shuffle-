a6e8abc994241be6e9cca2c8ca5b1f9285dd7325
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-11-21 00:51:10 +0300

    HomeWork32Function5(Shuffle)

---
+++ HomeWork32Function5(Shuffle)/Program.cs
@@ -0,0 +1,72 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int minValue = 0;
+        int maxValue = 100;
+        bool isWorking = true;
+        string userKey = "Enter";
+
+        Console.Write("Введите длину массива: ");
+        int arrayLength = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        Random random = new Random();
+
+        int[] array = new int[arrayLength];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(minValue, maxValue);
+
+            Console.Write(" " + array[i]);
+        }
+
+        Console.Write("\n\nНажмите " + userKey + " для перемешивания массива или любую клавишу для выхода\n\n");
+
+        while (isWorking)
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                ActionKey(key, ref isWorking);
+
+                if(isWorking == true)
+                {
+                    Shuffle(ref array);
+
+                    for (int i = 0; i < arrayLength; i++)
+                        Console.Write(" " + array[i]);
+                }
+            }
+        }
+    }
+
+    static void Shuffle(ref int[] array)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            int j = random.Next(array.Length);
+
+            int temporaryArray = array[j];
+            array[j] = array[i];
+            array[i] = temporaryArray;
+        }
+    }
+
+    static void ActionKey(ConsoleKeyInfo key, ref bool isWorking)
+    {
+        switch (key.Key)
+        {
+            case ConsoleKey.Enter:
+                isWorking = true;
+                break;
+            default:
+                isWorking = false;
+                break;
+        }
+    }
+}
