f7d9dd7ec5b95d4fa976474021f75414a1009ab4
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-11-21 11:27:35 +0300

    Fixed

--- HomeWork32Function5(Shuffle)/Program.cs
+++ HomeWork32Function5(Shuffle)/Program.cs
@@ -5,7 +5,6 @@
         int minValue = 0;
         int maxValue = 100;
         bool isWorking = true;
-        string userKey = "Enter";
 
         Console.Write("Введите длину массива: ");
         int arrayLength = Convert.ToInt32(Console.ReadLine());
@@ -22,51 +21,37 @@
             Console.Write(" " + array[i]);
         }
 
-        Console.Write("\n\nНажмите " + userKey + " для перемешивания массива или любую клавишу для выхода\n\n");
+        Console.Write("\n\nНажмите Enter для перемешивания массива или любую клавишу для выхода\n\n");
 
         while (isWorking)
         {
-            if (Console.KeyAvailable)
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (Console.KeyAvailable || key.Key == ConsoleKey.Enter)
             {
-                ConsoleKeyInfo key = Console.ReadKey();
+                Shuffle(array);
 
-                GetKey(key, ref isWorking);
-
-                if(isWorking == true)
-                {
-                    Shuffle(ref array);
-
-                    for (int i = 0; i < arrayLength; i++)
-                        Console.Write(" " + array[i]);
-                }
+                for (int i = 0; i < arrayLength; i++)
+                    Console.Write(" " + array[i]);
+            }
+            else
+            {
+                isWorking = false;
             }
         }
     }
 
-    static void Shuffle(ref int[] array)
+    static void Shuffle(int[] array)
     {
         Random random = new Random();
 
         for (int i = 0; i < array.Length; i++)
         {
-            int j = random.Next(array.Length);
+            int randomIndex = random.Next(array.Length);
 
-            int temporaryValue = array[j];
-            array[j] = array[i];
+            int temporaryValue = array[randomIndex];
+            array[randomIndex] = array[i];
             array[i] = temporaryValue;
-        }
-    }
-
-    static void GetKey(ConsoleKeyInfo key, ref bool isWorking)
-    {
-        switch (key.Key)
-        {
-            case ConsoleKey.Enter:
-                isWorking = true;
-                break;
-            default:
-                isWorking = false;
-                break;
         }
     }
 }
